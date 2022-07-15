8c7f119c523337a4a3674ecbcc4a2be63ecce38c
RedHeadphone <huzaifa.k@somaiya.edu>
2022-07-15 23:25:32 +0530

    added testcase genarator

---
+++ testcase_gen.py
@@ -0,0 +1,41 @@
+import random
+
+def random_string(n = None):
+    if n==None:
+        n = random.randint(5, 10)
+    print(n)
+    for _ in range(n):
+        print(chr(random.randint(97, 122)), end="")
+
+def random_array(n = None):
+    if n==None:
+        n = random.randint(5, 10)
+    print(n)
+    for _ in range(n):
+        print(random.randint(1, 10), end=" ")
+
+def random_binary_string(n = None):
+    if n==None:
+        n = random.randint(5, 10)
+    print(n)
+    for _ in range(n):
+        print(random.choice("01"), end="")
+
+def random_matrix(n=None, m=None):
+    if n==None:
+        n = random.randint(5, 10)
+    if m==None:
+        m = random.randint(5, 10)
+    print(n, m)
+    for _ in range(n):
+        for _ in range(m):
+            print(random.randint(1, 10), end=" ")
+        print()
+
+t = random.randint(5, 20)
+print(t)
+
+
+for _ in range(t):
+    
+    print()
