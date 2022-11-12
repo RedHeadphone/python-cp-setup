d315486d34e28c720b84190efec56a42dc65c3d5
RedHeadphone <huzaifa.k@somaiya.edu>
2022-11-12 21:17:29 +0530

    added ncr Fermat Little Theorem

--- main.py
+++ main.py
@@ -68,6 +68,14 @@ def is_prime(n):
             return False
     return True
 
+def ncr(n, r, p):
+    num = den = 1
+    for i in range(r):
+        num = (num * (n - i)) % p
+        den = (den * (i + 1)) % p
+    return (num * pow(den,
+            p - 2, p)) % p
+
 def case(t):
     print("Case #{}:".format(t), end=" ")
 
