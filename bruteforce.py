9b8eddc495c69acae731075fefaf26b8ecb10ca1
RedHeadphone <khilawala92@gmail.com>
2024-03-16 13:40:47 +0530

    bruteforce.py update

--- bruteforce.py
+++ bruteforce.py
@@ -1,11 +1,10 @@
-import random
-import math
+import sys, os, random, math
 from collections import defaultdict, Counter, deque, OrderedDict
 from heapq import heapify, heappush, heappop
 from functools import cache, reduce
 from bisect import bisect_left, bisect_right
 from types import GeneratorType
-import sys,os
+from typing import *
 
 input = lambda : sys.stdin.readline().strip()
 
@@ -84,9 +83,6 @@ def ncr(n, r, p):
     return (num * pow(den,
             p - 2, p)) % p
 
-def case(t):
-    print("Case #{}:".format(t), end=" ")
-
 # For codeforces - hashmap to avoid TLE
 RANDOM = random.randrange(2**62)
 def mapping_wrapper(x):
@@ -153,6 +149,85 @@ def bootstrap(f):
             return to
     return wrappedfunc
 
+def binary_search(left,right,check,ans):
+    minimum_ans = (ans == left)
+    while left<=right:
+        mid = (left+right)//2
+        if minimum_ans:
+            if check(mid):
+                ans,left = mid, mid+1
+            else:
+                right = mid-1
+        else:
+            if check(mid):
+                ans,right = mid, mid-1
+            else:
+                left = mid+1
+    return ans
+
+binn = lambda num: bin(num)[2:]
+
+class lazyheap:
+    def __init__(self):
+        self.heap = []  
+        self.count = 0   
+        self.sum = 0 
+        self.toremove = Counter()
+    
+    def push(self, item):
+        heappush(self.heap, item)
+        self.count += 1
+        self.sum += item
+ 
+    def remove(self, item):
+        self.toremove[item] += 1    
+        self.count -= 1
+        self.sum -= item
+ 
+    def top(self):
+        x = self.heap[0]
+        while self.toremove[x] > 0:
+            self.toremove[x] -= 1
+            heappop(self.heap)
+            x = self.heap[0]
+        return x
+ 
+    def pop(self):
+        x = self.top()
+        heappop(self.heap)
+        self.count -= 1
+        self.sum -= x
+        return x
+
+def bit_sum(num):
+    b = len(binn(num))
+    bs = [0] * b
+    c = 2**b - 1
+    for i in range(b):
+        c -= (1<<i)
+        bs[i] = (num & c) // 2
+        if num & ((1<<i)):
+            bs[i] += (num % (1<<i)) + 1
+    return bs
+
+class RollingHash:
+    def __init__(self, base = 256, string = "", func = ord):
+        self.base = base
+        self.hash = 0
+        self.length = 0
+        self.func = func
+        for i in string:
+            self.left_add(i)
+
+    def left_add(self, char):
+        self.hash =  (self.hash * self.base + self.func(char)) % MOD
+        self.length += 1
+    
+    def right_add(self, char):
+        self.hash =  (self.hash + self.func(char) * pow(self.base, self.length, MOD)) % MOD
+        self.length += 1
+
+
 ###############################################################################
 
 def solve(case=None):
@@ -160,6 +235,7 @@ def solve(case=None):
 
 ###############################################################################
 
-for t in range(int(input())):
-    # case(t+1)
+test_cases = int(input())
+# test_cases = 1
+for t in range(test_cases):
     solve(t+1)
