5144209bb0d346b990f26703f56fc6cf9a7a209d
RedHeadphone <khilawala92@gmail.com>
2024-03-25 13:21:55 +0530

    compact fenwick tree class

--- sorted_list.py
+++ sorted_list.py
@@ -5,20 +5,18 @@ from bisect import bisect_left, bisect_right
 class FenwickTree:
     def __init__(self, x):
         bit = self.bit = list(x)
-        size = self.size = len(bit)
-        for i in range(size):
+        bit_size = self.bit_size = len(bit)
+        for i in range(bit_size):
             j = i | (i + 1)
-            if j < size:
+            if j < bit_size:
                 bit[j] += bit[i]
 
     def update(self, idx, x):
-        """updates bit[idx] += x"""
-        while idx < self.size:
+        while idx < self.bit_size:
             self.bit[idx] += x
             idx |= idx + 1
 
     def __call__(self, end):
-        """calc sum(bit[:end])"""
         x = 0
         while end:
             x += self.bit[end - 1]
@@ -26,11 +24,10 @@ class FenwickTree:
         return x
 
     def find_kth(self, k):
-        """Find largest idx such that sum(bit[:idx]) <= k"""
         idx = -1
-        for d in reversed(range(self.size.bit_length())):
+        for d in reversed(range(self.bit_size.bit_length())):
             right_idx = idx + (1 << d)
-            if right_idx < self.size and self.bit[right_idx] <= k:
+            if right_idx < self.bit_size and self.bit[right_idx] <= k:
                 idx = right_idx
                 k -= self.bit[idx]
         return idx + 1, k
