90c43e9638f8ac9252c231a852ef3a5c4307fe50
RedHeadphone <khilawala92@gmail.com>
2024-05-25 14:34:06 +0530

    MergeSortTree + bit_length + format

--- main.py
+++ main.py
@@ -18,8 +18,10 @@ def debug(*x, **y):
 
 ###############################################################################
 
+
 def solve_bruteforce(case=None):
     pass
+
 
 def solve(case=None):
     pass
@@ -34,11 +36,69 @@ TRUE_MAPPING, FALSE_MAPPING = "YES", "NO"
 ###############################################################################
 
 
-def principal_of_inclusion_exclusion(arr,func):
+class MergeSortTree:
+
+    def __init__(self, arr):
+        self.log = len(arr).bit_length()
+        if len(arr) == 1 << (self.log - 1):
+            self.log -= 1
+        self.n = 1 << self.log
+        self.tree = [[0] * self.n for _ in range(self.log + 1)]
+        self.csum = [[0] * self.n for _ in range(self.log + 1)]
+
+        for depth in range(self.log + 1):
+            for i in range(len(arr)):
+                self.tree[depth][i] = arr[i]
+
+        for depth in range(self.log + 1):
+            w = 1 << depth
+            for i in range(self.n // w):
+                self.tree[depth][i * w : (i + 1) * w] = sorted(
+                    self.tree[depth][i * w : (i + 1) * w]
+                )
+                self.csum[depth][i * w : (i + 1) * w] = list(
+                    itertools.accumulate(self.tree[depth][i * w : (i + 1) * w])
+                )
+
+    def query(self, l, r, x):
+        separate = []
+        ret = 0
+        for depth in range(self.log + 1):
+            size = 1 << depth
+            if r - l < size:
+                break
+            if l % (2 * size) == 0:
+                continue
+            separate.append((l, l + size))
+            l += size
+
+        for depth in range(self.log, -1, -1):
+            size = 1 << depth
+            if r - l >= size:
+                separate.append((l, l + size))
+                l += size
+
+        for l, r in separate:
+            depth = (r - l).bit_length() - 1
+            ok, ng = l - 1, r
+            while ng - ok > 1:
+                m = (ng + ok) // 2
+                if self.tree[depth][m] <= x:
+                    ok = m
+                else:
+                    ng = m
+            index = ng - 1
+            if index < l:
+                continue
+            ret += self.csum[depth][index]
+        return ret
+
+
+def principal_of_inclusion_exclusion(arr, func):
     n = len(arr)
     for j in range(n):
         multiplier = (-1) ** j
-        for comb in itertools.combinations(arr,j+1):
+        for comb in itertools.combinations(arr, j + 1):
             yield (func(comb), multiplier)
 
 
@@ -264,6 +324,9 @@ def binary_search(left, right, check, start_from_left):
 
 
 class BitManipulation:
+    def bit_length(self, num):
+        return num.bit_length()
+
     def bin(self, num, size=None):
         bin_string = bin(num)[2:]
         return bin_string if (size == None) else bin_string.zfill(size)
@@ -480,7 +543,7 @@ class SortedList:
         return str(list(self))
 
 
-for t in range(NUMBER_OF_TEST_CASES if NUMBER_OF_TEST_CASES <=1 else int(input())):
+for t in range(NUMBER_OF_TEST_CASES if NUMBER_OF_TEST_CASES <= 1 else int(input())):
     if BOOLEAN_RETURN:
         print(TRUE_MAPPING if solve(t + 1) else FALSE_MAPPING)
     else:
